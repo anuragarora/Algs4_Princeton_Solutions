816a8be2f416131783af0fd2bdef9370db25b6f5
Anurag Arora <anurag.arora@outlook.com>
2018-03-04 14:58:14 -0700

    deleting deque file from root

--- Driver.java
+++ Driver.java
@@ -2,7 +2,116 @@ public class Driver {
     public static void main(String args[]){
         Driver driver = new Driver();
         //driver.testFixedCapacityQueue(10);
-        driver.testResizableArrayQueue();
+        //driver.testResizableArrayQueue();
+        //driver.testDeque();
+        //driver.testRandomizedQueue();
+        driver.testDeque2();
+    }
+    
+    public void testDeque2() {
+        Deque<Integer> deque = new Deque<>();
+        deque.addLast(0);
+        System.out.println(deque.removeLast());
+        deque.addLast(2);
+        System.out.println(deque.removeLast());
+        deque.addLast(4);
+        System.out.println(deque.removeLast());
+        deque.addLast(6);
+        System.out.println(deque.isEmpty());
+        System.out.println(deque.removeLast());
+        System.out.println(deque.isEmpty());
+    }
+    
+    public void testRandomizedQueue() {
+        RandomizedQueue<String> queue = new RandomizedQueue<>();
+        queue.enqueue("Anurag");
+        queue.enqueue("Arora");
+        queue.enqueue("A");
+        queue.enqueue("B");
+        queue.enqueue("C");
+        queue.enqueue("D");
+        queue.enqueue("E");
+        queue.enqueue("F");
+        queue.enqueue("G");
+        queue.enqueue("H");
+        queue.enqueue("I");
+        queue.enqueue("J");
+        queue.enqueue("K");
+        queue.enqueue("L");
+        
+        // Printing the collection through an iterator
+        for (String string : queue){
+            System.out.println(string);
+        }
+        
+        System.out.println("\ndeQueuing items :\n");
+        System.out.println(queue.dequeue());
+        System.out.println(queue.dequeue());
+        System.out.println(queue.dequeue());
+        System.out.println(queue.dequeue());
+        System.out.println(queue.dequeue());
+        System.out.println(queue.dequeue());
+        System.out.println(queue.dequeue());
+        
+        System.out.println("\nEnqueuing more items :\n");
+        
+        queue.enqueue("P");
+        queue.enqueue("Q");
+        queue.enqueue("R");
+        queue.enqueue("S");
+        queue.enqueue("T");
+        queue.enqueue("U");
+        queue.enqueue("V");
+        
+        for (String string : queue) {
+            System.out.println(string);
+        }
+    }
+    
+    public void testDeque() {
+        Deque<String> deque = new Deque<>();
+        deque.addFirst("Anurag");
+        deque.addLast("Arora");
+        deque.addFirst("A");
+        deque.addLast("B");
+        deque.addFirst("C");
+        deque.addFirst("D");
+        deque.addLast("E");
+        deque.addFirst("F");
+        deque.addLast("G");
+        deque.addLast("H");
+        deque.addLast("I");
+        deque.addLast("J");
+        deque.addFirst("K");
+        deque.addFirst("L");
+        
+        // Printing the collection through an iterator
+        for (String string : deque){
+            System.out.println(string);
+        }
+        
+        System.out.println("\ndeQueuing items :\n");
+        System.out.println(deque.removeFirst());
+        System.out.println(deque.removeLast());
+        System.out.println(deque.removeFirst());
+        System.out.println(deque.removeLast());
+        System.out.println(deque.removeFirst());
+        System.out.println(deque.removeLast());
+        System.out.println(deque.removeFirst());
+        
+        System.out.println("\nEnqueuing more items :\n");
+        
+        deque.addFirst("P");
+        deque.addLast("Q");
+        deque.addFirst("R");
+        deque.addLast("S");
+        deque.addFirst("T");
+        deque.addLast("U");
+        deque.addFirst("V");
+        
+        for (String string : deque) {
+            System.out.println(string);
+        }
     }
     
     public void testResizableArrayQueue() {
